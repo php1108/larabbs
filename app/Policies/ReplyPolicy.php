cd1dfb02eb4aeba7eecadfd8dae75f5c83f1ae50
zhouwengang <php1108@126.com>
2017-12-23 09:49:11 +0800

    User can delete reply

--- app/Policies/ReplyPolicy.php
+++ app/Policies/ReplyPolicy.php
@@ -10,7 +10,8 @@ class ReplyPolicy extends Policy
     public function update(User $user, Reply $reply)
     {
         // return $reply->user_id == $user->id;
-        return true;
+        return $user->isAuthorOf($reply) || $user->isAuthorOf($reply->topic);
+//        return true;
     }
 
     public function destroy(User $user, Reply $reply)
